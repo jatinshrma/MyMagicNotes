b829af90e2ce31458ff0cf038a685ff42f05658b
Jatin Sharma <jatin2907sharma@gmail.com>
2021-10-28 15:12:41 +0530

    Updated UI

--- Notes.js
+++ Notes.js
@@ -76,10 +76,10 @@ function showNotes() {
     notesObj.forEach(function (element, index) {
         html += `
         <div class="card my-2 mx-2 notecard" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title" id= "${index}title"></h5>
-          <p class="card-text">${element}</p>
-          <button id="${index}" onClick="delNote(this.id)" class="btn btn-primary">Delete Note</button>
+          <div class="card-body">
+            <h5 class="card-title" id= "${index}title"></h5>
+            <p class="card-text">${element}</p>
+            <button id="${index}" onClick="delNote(this.id)" class="btn btn-primary">Delete Note</button>
           </div>
         </div>
         `
@@ -158,17 +158,9 @@ searchTxt.addEventListener('input', function () {
     inputVal = searchTxt.value;
     let cardBody = document.getElementsByClassName('notecard');
     Array.from(cardBody).forEach(function (element) {
-        let cardTxt = element.getElementsByTagName('p')[0].innerText;
-        if (cardTxt.includes(inputVal)) {
-            element.style.display = 'block';
-        }
-        else {
-            element.style.display = 'none';
-        }
-    });
-    Array.from(cardBody).forEach(function (element) {
-        let cardTitle = element.getElementsByTagName('h5')[0].innerText;
-        if (cardTitle.includes(inputVal)) {
+        let titleTxt = element.getElementsByTagName('h5')[0].innerText;
+        let descriptTxt = element.getElementsByTagName('p')[0].innerText;
+        if (descriptTxt.includes(inputVal.toLowerCase()) || titleTxt.includes(inputVal.toLowerCase())) {
             element.style.display = 'block';
         }
         else {
